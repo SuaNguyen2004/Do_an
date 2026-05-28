67c89b2b54680656e20f0b8e2d356501ef1b0099
‘SuaNguyen2004’ <abc@gmail.com>
2026-05-28 22:56:35 +0700

    sửa lại 1 chút cho nút trang admin

--- admin/view/header.php
+++ admin/view/header.php
@@ -16,17 +16,17 @@
         <div class="bg-secondary-subtle p-5">
             <h1 class="fw-bold mb-4">TRANG CHỦ ADMIN</h1>
             <div class="ms-5">
-                <button class="btn btn-info me-2 mb-2 mb-sm-0">
+                <button class="btn btn-primary me-2 mb-2 mb-sm-0">
                     <a href="index.php" class="text-decoration-none text-dark fw-bold">Trang chủ</a>
                 </button>
-                <button class="btn btn-warning me-2 mb-2 mb-sm-0">
+                <button class="btn btn-light me-2 mb-2 mb-sm-0">
                     <a href="index.php?act=danhmuc" class="text-decoration-none text-dark fw-bold">Danh mục</a>
                 </button>
-                <button class="btn btn-success me-2 mb-2 mb-sm-0">
-                    <a href="index.php?act=sanpham" class="text-decoration-none text-light fw-bold">Sản phẩm</a>
+                <button class="btn btn-light me-2 mb-2 mb-sm-0">
+                    <a href="index.php?act=sanpham" class="text-decoration-none text-dark fw-bold">Sản phẩm</a>
                 </button>
-                <button class="btn btn-primary me-2">
-                    <a href="index.php?act=user" class="text-decoration-none text-light fw-bold">Tài khoản</a>
+                <button class="btn btn-light me-2">
+                    <a href="index.php?act=user" class="text-decoration-none text-dark fw-bold">Tài khoản</a>
                 </button>
                 <button class="btn btn-danger me-2">
                     <a href="index.php?act=thoat" class="text-decoration-none text-light fw-bold text-center">Đăng
